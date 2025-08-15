182ab95a11fd808be1dc9559289a8aa021a5b691
RHofmann <rhofmann@armadale.wa.gov.au>
2025-08-15 14:50:50 +0800

    styling and tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    libInjectCss(),
+    tailwindcss(),
     dts({ include: ['lib'], tsconfigPath: './tsconfig.app.json' })
   ],
   resolve: {
@@ -16,6 +20,9 @@ export default defineConfig({
   },
   build: {
     copyPublicDir: false,
+    rollupOptions: {
+      external: ['vue'],
+    },
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       name: 'SherlocksSanityLibrary',
