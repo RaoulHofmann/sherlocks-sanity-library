f5cc7f6399e749ffbc742eebe44b8204c7e15b04
Raoul <raouldhofmann@gmail.com>
2025-08-29 21:06:39 +0800

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -6,30 +6,30 @@ import {libInjectCss} from "vite-plugin-lib-inject-css";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        libInjectCss(),
-        dts({include: ['lib'], tsconfigPath: './tsconfig.app.json'})
-    ],
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "lib")
-        }
-    },
-    optimizeDeps: {
-      include: [
-        '@sanity/image-url',
-      ],
-    },
-    build: {
-        copyPublicDir: false,
-        rollupOptions: {
-            external: ['vue', '@portabletext/vue', '@sanity/image-url'],
-        },
-        lib: {
-            entry: resolve(__dirname, 'lib/main.ts'),
-            name: 'SherlocksSanityLibrary',
-            formats: ['es']
-        }
+  plugins: [
+    vue(),
+    libInjectCss(),
+    dts({include: ['lib'], tsconfigPath: './tsconfig.app.json'})
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "lib")
     }
+  },
+  optimizeDeps: {
+    include: [
+      '@sanity/image-url',
+    ],
+  },
+  build: {
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ['vue', '@portabletext/vue', '@sanity/image-url'],
+    },
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'SherlocksSanityLibrary',
+      formats: ['es']
+    }
+  }
 })
